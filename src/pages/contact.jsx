025e9dca7a95dd5595cf1c3b97c88a625da59b01
Jadom <quaddomdev@gmail.com>
2024-02-10 16:43:27 -0300

    [update] form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { useState } from 'react';
 import { PageContainer } from '../components/PageContainer';
 import { Button, Input } from '@nextui-org/react';
+import '../styles/contact.scss';
 
 export const Contact = () => {
   const handleSubmit = () => {};
 
   return (
     <PageContainer>
-      <h1>Contact</h1>
       <ContactForm />
     </PageContainer>
   );
@@ -36,37 +36,45 @@ const ContactForm = () => {
   };
 
   return (
-    <div>
-      <h2>Formulario de contacto</h2>
-      <form onSubmit={handleSubmit}>
-        <Input
-          label="Nombre"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <Input
-          label="Apellidos"
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <Input
-          label="Email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <Input
-          label="Mensaje"
-          type="textarea"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          required
-        />
+    <div className="containerAll">
+      <form onSubmit={handleSubmit} className="contactForm">
+        <h2>Formulario de contacto</h2>
+        <div className="inputContainer">
+          <Input
+            label="Nombre"
+            type="text"
+            className="input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <Input
+            label="Apellidos"
+            type="text"
+            className="input"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div className="inputContainer">
+          <Input
+            label="Email"
+            type="email"
+            className="input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <Input
+            label="Mensaje"
+            type="textarea"
+            className="input"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+        </div>
         <Button color="primary">Enviar</Button>
       </form>
     </div>
